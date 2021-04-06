51dc6c4ba3012b7a0db2ba5a9c01c5e957a25c94
Mturner7 <80474793+Mturner7@users.noreply.github.com>
2021-04-06 13:47:20 -0400

    Adjusted output

--- RockPaperScissors/Game.cs
+++ RockPaperScissors/Game.cs
@@ -30,19 +30,47 @@ namespace RockPaperScissors
                 input = Console.ReadLine().ToLower();
 
                 if (input == "l" || input == "longboywolfie98") return new Longboywolfie98();
-               
+
 
             } while ((input != "d" && input != "dynamo977"));
-          
+
             return new Dynamo977(new Random());
         }
 
+        private static bool CheckForWinner(Player thePlayer, Player Opponent)
+        {
+            int playerChoice = (int)thePlayer.GenerateRoShamBo();
+            int oppChoice = (int)Opponent.GenerateRoShamBo();
+
+            Console.WriteLine($"\n{Opponent.Name}'s Move: {((Player.Values)oppChoice)}");
+            Console.WriteLine($"{thePlayer.Name}'s Move: {((Player.Values)playerChoice)}\n");
+
+            if ((oppChoice == 2 && playerChoice == 1) || (oppChoice == 3 && playerChoice == 2) || (oppChoice == 1 && playerChoice == 3))
+            {
+                Opponent.Wins++;
+                Console.WriteLine($"{Opponent.Name} wins!");
+            }
+            else if (playerChoice == oppChoice)
+            {
+                Console.WriteLine("Tie! Nobody wins!");
+                return false;
+            }
+            else
+            {
+                thePlayer.Wins++;
+                Console.WriteLine($"{thePlayer.Name} wins!");
+            }
+
+            return true;
+        }
 
         public static void PlayGame()
         {
+            int ties = 0;
             int gameCount = 0;
             bool playing = true;
-            
+            bool DifferentOpponent = false;
+
             Console.WriteLine($"Welcome to Ro Sham Bo!");
 
             Console.Write("\nWhat's your name buckaroo? ");
@@ -52,33 +80,29 @@ namespace RockPaperScissors
 
             while (playing)
             {
+                
                 Console.Clear();
                 Console.WriteLine($"| Game: {++gameCount} |");
+                Console.WriteLine($"| Ties: {ties} |");
                 Console.WriteLine($"| {thePlayer.Name}'s total wins: {thePlayer.Wins} |");
                 Console.WriteLine($"| {Opponent.Name}'s total wins: {Opponent.Wins} | \n");
 
-                int playerChoice = (int)thePlayer.GenerateRoShamBo();
-                int oppChoice = (int)Opponent.GenerateRoShamBo();
+                if (!CheckForWinner(thePlayer, Opponent)) ties++; // If nobody wins, increment the total of amount of ties
 
-                Console.WriteLine();
-                Console.WriteLine($"{Opponent.Name}'s Move: {((Player.Values)oppChoice)}");
-                Console.WriteLine($"{thePlayer.Name}'s Move: {((Player.Values)playerChoice)}\n");
+                playing = ContinuePrompt($"Would you like to play {Opponent.Name} again?");
 
-                if ((oppChoice == 2 && playerChoice == 1) || (oppChoice == 3 && playerChoice == 2) || (oppChoice == 1 && playerChoice == 3))
+                if (!playing) // Prompt the user if they want to play a different opponent before exiting
                 {
-                    Opponent.Wins++;
-                    Console.WriteLine($"{Opponent.Name} wins!");
-                }
-                
-                else if (playerChoice == oppChoice) Console.WriteLine("\nTie! Nobody wins!");
-                
-                else
-                {
-                    thePlayer.Wins++;
-                    Console.WriteLine($"{thePlayer.Name} wins!");
+                    DifferentOpponent = ContinuePrompt("Would you like play against a different opponent?");
+                    playing = DifferentOpponent;
                 }
 
-                playing = ContinuePrompt("Would you like to play again?");
+                if(DifferentOpponent) // If the user chooses to continue with a different opponent, reset all stats
+                {
+                    thePlayer.Wins = gameCount = ties = 0;
+                    Opponent = ChooseOpponent();
+                }
+
             }
         }
 
